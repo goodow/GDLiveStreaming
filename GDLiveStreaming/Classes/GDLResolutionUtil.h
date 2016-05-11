e2244a6e6b18911153ab08df6bb0d04079abcd7c
Larry Tin <dev@goodow.com>
2016-05-11 17:40:09 +0800

    declare video quality constants, define resolution and bitrate range

--- GDLiveStreaming/Classes/GDLResolutionUtil.h
+++ GDLiveStreaming/Classes/GDLResolutionUtil.h
@@ -5,6 +5,27 @@
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
 
+struct GDLRange {
+  CGFloat min;
+  CGFloat max;
+};
+typedef struct GDLRange GDLRange;
+struct GDLResolution {
+  CGSize size;
+  struct GDLRange bitrate;
+};
+typedef struct GDLResolution GDLResolution;
+
+extern const struct GDLResolution GDLResolution1440p60fps;
+extern const struct GDLResolution GDLResolution1440p;
+extern const struct GDLResolution GDLResolution1080p60fps;
+extern const struct GDLResolution GDLResolution1080p;
+extern const struct GDLResolution GDLResolution720p60fps;
+extern const struct GDLResolution GDLResolution720p;
+extern const struct GDLResolution GDLResolution480p;
+extern const struct GDLResolution GDLResolution360p;
+extern const struct GDLResolution GDLResolution240p;
+
 @interface GDLResolutionUtil : NSObject
 + (CGSize)videoSize:(NSString *)sessionPreset isPortrait:(BOOL)isPortrait;
 
--- GDLiveStreaming/Classes/GDLResolutionUtil.m
+++ GDLiveStreaming/Classes/GDLResolutionUtil.m
@@ -4,6 +4,43 @@
 
 #import "GDLResolutionUtil.h"
 
+// https://support.google.com/youtube/answer/2853702
+const struct GDLResolution GDLResolution1440p60fps = {
+    .size = {.width = 2560.f, .height = 1440.f},
+    .bitrate = {.min = 9000, .max = 18000}
+};
+const struct GDLResolution GDLResolution1440p = {
+    .size = {.width = 2560.f, .height = 1440.f},
+    .bitrate = {.min = 6000, .max = 13000}
+};
+const struct GDLResolution GDLResolution1080p60fps = {
+    .size = {.width = 1920.f, .height = 1080.f},
+    .bitrate = {.min = 4500, .max = 9000}
+};
+const struct GDLResolution GDLResolution1080p = {
+    .size = {.width = 1920.f, .height = 1080.f},
+    .bitrate = {.min = 3000, .max = 6000}
+};
+const struct GDLResolution GDLResolution720p60fps = {
+    .size = {.width = 1280.f, .height = 720.f},
+    .bitrate = {.min = 2250, .max = 6000}
+};
+const struct GDLResolution GDLResolution720p = {
+    .size = {.width = 1280.f, .height = 720.f},
+    .bitrate = {.min = 1500, .max = 4000}
+};
+const struct GDLResolution GDLResolution480p = {
+    .size = {.width = 854.f, .height = 480.f},
+    .bitrate = {.min = 500, .max = 2000}
+};
+const struct GDLResolution GDLResolution360p = {
+    .size = {.width = 640.f, .height = 360.f},
+    .bitrate = {.min = 400, .max = 1000}
+};
+const struct GDLResolution GDLResolution240p = {
+    .size = {.width = 426.f, .height = 240.f},
+    .bitrate = {.min = 300, .max = 700}
+};
 
 @implementation GDLResolutionUtil {
 
